6f40157ce0b479612f9319672a747073b8bd2a8a
Desperos <yakovsl@ya.ru>
2022-11-20 20:53:05 +0300

    2

--- sprintf.c
+++ sprintf.c
@@ -1,17 +1,48 @@
 #include "sprintf.h"
 
-int s21_sprintf(char *str, const char *format, ...);
+void s21_sprintf(char *str, const char *format, ...);
+void init_struct(opt *opt);
 
 int main(void) {
-  char *string[1024] = {};
-  s21_sprintf(string, "%d", "A");
-  printf("%s\n", string);
+  char string[1024] = {};
+  sprintf(string, "%d", 1);
+  LOG_INFO("sprintf orig str %s, len %ld", string, strlen(string));
+  s21_sprintf(string, "%sdada");
   return 0;
 }
 
-int s21_sprintf(char *str, const char *format, ...) {
+void s21_sprintf(char *str, const char *format, ...) {
+  (void)str;
+  opt opt = {};
   setlocale(LC_ALL, "");
   setlocale(LC_ALL, "en_US.UTF-8");
+  va_list arg;
+  va_start(arg, format);
+  int agr_len = STR_LEN(format);
+  LOG_INFO("Длинна строки %d, строка \"%s\"", agr_len, format);
+  for (int i = 0; i < agr_len; i++) {
+    if (format[i] == '%') {
+      init_struct(&opt);
+      for (int j = 0; j == 1; i++) {
+      }
+    }
+  }
+  va_end(arg);
+}
 
-
+void init_struct(opt *opt) {
+  opt->accuracy = 0;
+  opt->conversion = 0;
+  opt->flags.grid = 0;
+  opt->flags.minus = 0;
+  opt->flags.plus = 0;
+  opt->flags.space = 0;
+  opt->flags.zero = 0;
+  opt->modifiers.h = 0;
+  opt->modifiers.hh = 0;
+  opt->modifiers.l = 0;
+  opt->modifiers.ll = 0;
+  opt->modifiers.L = 0;
+  opt->width = 0;
+  opt->conversion = 0;
 }
--- sprintf.h
+++ sprintf.h
@@ -4,11 +4,36 @@
 #include <stdarg.h>
 #include <stdio.h>  //удалить
 #include <stdlib.h>
+#include <string.h>
 #include <wchar.h>
+#define DEBUG
 
 #ifdef DEBUG
-#define log_info(M, ...) \
+#define LOG_INFO(M, ...) \
   fprintf(stderr, "[INFO] (%s:%d) " M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+#define STR_LEN strlen
 #else
-#define log_info(M, ...)
+#define LOG_INFO(M, ...)
+#define STR_LEN s21_strlen
 #endif
+
+//[flags][width][.precision][size]conversion
+typedef struct sprintf {
+  struct {
+    int minus;
+    int plus;
+    int space;
+    int grid;
+    int zero;
+  } flags;
+  int width;
+  int accuracy;
+  struct {
+    int h;
+    int hh;
+    int l;
+    int ll;
+    int L;
+  } modifiers;
+  char conversion;
+} opt;
