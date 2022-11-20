4cf1033bfaad2c1a7b32ca4acbfd08c3bca95e12
Desperos <yakovsl@ya.ru>
2022-11-20 16:21:35 +0300

    1

--- sprintf.c
+++ sprintf.c
@@ -3,10 +3,12 @@
 int s21_sprintf(char *str, const char *format, ...);
 
 int main(void) {
-  char test_int = 0;
-  sprintf(test_int, "%d", "1");
-  printf("%d\n", &test_int);
-  // s21_sprintf(test_int, "%c", "1");
-  // printf("%s\n", &test_int);
+  char *string[1024] = {};
+  s21_sprintf(string, "%d", "A");
+  printf("%s\n", string);
   return 0;
+}
+
+int s21_sprintf(char *str, const char *format, ...) {
+
 }
--- sprintf.h
+++ sprintf.h
@@ -5,3 +5,10 @@
 #include <stdio.h>  //удалить
 #include <stdlib.h>
 #include <wchar.h>
+
+#ifdef DEBUG
+#define log_info(M, ...) \
+  fprintf(stderr, "[INFO] (%s:%d) " M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+#else
+#define log_info(M, ...)
+#endif
